41fcf18865d1c33a78383e96a5a5c0540a66690a
Hanna Lizarraga <siddharttha95@gmail.com>
2020-07-13 19:40:18 -0500

    onclick

--- app/src/main/java/com/android/helloworld/MainActivity.kt
+++ app/src/main/java/com/android/helloworld/MainActivity.kt
@@ -4,55 +4,53 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.CheckBox
-import android.widget.Toast
+import android.widget.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
-        Log.i("LOGCICLO","Entramos en metodo onCreate")
+
+        val btn1:Button = findViewById(R.id.button_1)
+        btn1.setOnClickListener{ view ->
+            Toast.makeText(this, "Click en el boton 1", Toast.LENGTH_SHORT).show()
+        }
+            val tv2: TextView = findViewById(R.id.textView_2)
+        tv2.setOnClickListener(this)
+        }
+
+    override fun onClick(v: View?) {
+        Toast.makeText(this, "Click en el texto 2", Toast.LENGTH_SHORT).show()
     }
-
-    override fun onStart() {
-        super.onStart()
-        Log.i("LOGCICLO","Entramos en metodo onStart")
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i("LOGCICLO","Entramos en metodo onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i("LOGCICLO","Entramos en metodo onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i("LOGCICLO","Entramos en metodo onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("LOGCICLO","Entramos en metodo onDestroy")
-    }
-
     fun clickOnFruta(view: View) {
         val checkBox: CheckBox = view as CheckBox
         val checked: Boolean = checkBox.isChecked
-        when(view.id){
+        when (view.id) {
             R.id.checkbox_frutas -> {
-                if (checked){
-                    Toast.makeText(this,"Se ha hecho click en Frutas", Toast.LENGTH_LONG).show()
+                if (checked) {
+                    Toast.makeText(this, "Se ha hecho click en Frutas", Toast.LENGTH_LONG).show()
 
-                }else{
+                } else {
                     Toast.makeText(this, "Se ha desmarcado Fruta", Toast.LENGTH_LONG).show()
+                }
             }
         }
+    }
+
+    fun sexoSeleccionado(view: View) {
+        val radioButtonSeleccionado = view as RadioButton
+
+        when (view.id) {
+            R.id.radioButtonMujer -> {
+                Toast.makeText(this, "Se ha seleccionado Mujer", Toast.LENGTH_SHORT).show()
+            }
+            R.id.radioButtonHombre -> {
+                Toast.makeText(this, "Se ha seleccionado Hombre", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
 }
+
+
+
